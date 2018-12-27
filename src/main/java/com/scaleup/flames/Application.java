16867394622e3ce181c5188ff8fbcb7303cd0bdd
brijeshkumar <brijeshkumar1504@gmail.com>
2018-12-27 12:52:31 +0530

    updated the main class

--- src/main/java/com/scaleup/flames/Application.java
+++ src/main/java/com/scaleup/flames/Application.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FlamesApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlamesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
-
